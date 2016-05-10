cea05698fda28025ba4a6342280f17dc4ea3a6cc
fericcio <fe.riccio@gmail.com>
2016-05-10 17:21:33 +0630

    Update media.py

--- media.py
+++ media.py
@@ -3,30 +3,31 @@ import webbrowser
 class Video():
 	"""docstring for video: this class contains information about the title, duration of the movie and its votes"""
 
-	def __init__(self, movie_title, votes):
+	def __init__(self, movie_title):
+		"""Common data shared by both the Movie and TV_show class"""
 		self.title = movie_title
-		self.votes = votes
+#		self.votes = votes
 		
 class Movie(Video):
     """This class provide a way to store movie related information"""
     
-    def __init__(fede, movie_storyline, poster_image, trailer_youtube):
-            Video.__init__(self, movie_title,votes)
-            fede.storyline = movie_storyline
-            fede.poster_image_url = poster_image
-            fede.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(fede):
-            webbrowser.open(fede.trailer_youtube_url)
-
-class TV_show(Video):
-	"""This class provides a way to store tv shows related information"""
-	def __init___(self, tv_show_image, nr_of_seasons, tv_station):
-		Video.__init__(self, movie_title, votes):
-		self.tv_show_image = tv_show_image
-		self.nr_of_seasons = nr_of_seasons
-		self.tv_station = tv_station
+    def __init__(self, movie_title, votes, movie_storyline, poster_image, trailer_youtube):
+        Video.__init__(self, movie_title)
+        self.storyline_text = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url) 
+            webbrowser.open(fede.trailer_youtube_url)
 
+class Tv_show(Video):
+	"""This class provides a way to store tv shows related information"""
+	def __init__(self, movie_title, tv_show_image, trailer_youtube):
+		Video.__init__(self, movie_title)
+		self.tv_show_image_url = tv_show_image
+		self.trailer_youtube_url = trailer_youtube
+#		self.nr_of_seasons = nr_of_seasons
+#		self.tv_station = tv_station
+
+#	def show_trailer(self):
+#		webbrowser.open(self.trailer_youtube_url) 
