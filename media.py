e73a7c4e30459eb7856a85f193252be3d0740ddf
fericcio <fe.riccio@gmail.com>
2016-05-10 23:02:09 +0630

    Update media.py

--- media.py
+++ media.py
@@ -11,23 +11,23 @@ class Video():
 class Movie(Video):
     """This class provide a way to store movie related information"""
     
-    def __init__(self, movie_title, votes, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, votes, movie_storyline, poster_image, movie_trailer_youtube):
         Video.__init__(self, movie_title)
         self.storyline_text = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.trailer_youtube_url = movie_trailer_youtube
 
     def show_trailer(self):
             webbrowser.open(fede.trailer_youtube_url)
 
 class Tv_show(Video):
 	"""This class provides a way to store tv shows related information"""
-	def __init__(self, movie_title, tv_show_image, trailer_youtube):
+	def __init__(self, movie_title, tv_show_image, tv_show_trailer_youtube):
 		Video.__init__(self, movie_title)
 		self.tv_show_image_url = tv_show_image
-		self.trailer_youtube_url = trailer_youtube
+		self.trailer_youtube_url = tv_show_trailer_youtube
 #		self.nr_of_seasons = nr_of_seasons
 #		self.tv_station = tv_station
 
-#	def show_trailer(self):
-#		webbrowser.open(self.trailer_youtube_url) 
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url) 
